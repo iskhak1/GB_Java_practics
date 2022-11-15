fa8c48eb63c6371e0740d34270d4166cd137a3c7
Iskhak <isakask12242001@gmail.com>
2022-11-15 20:53:35 +0300

    features: ex1 well done)

---
+++ src/main/java/ru/gb/home4/MyHomeWork.java
@@ -0,0 +1,30 @@
+package ru.gb.home4;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyHomeWork {
+
+
+    public static void main(String[] args) {
+        List<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+        linkedList.add(6);
+        linkedList.add(7);
+        System.out.println(linky((LinkedList) linkedList));
+    }
+
+    private static List linky(LinkedList linkedList) {
+        LinkedList list = new LinkedList();
+        for (int i = 0; i <= linkedList.size()-1; i++) {
+            list.add(linkedList.size()-i);
+        }
+
+        return list;
+    }
+}
