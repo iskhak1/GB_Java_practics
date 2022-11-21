bd17fd1947da472a0d8ce3afcf3e1c3c03d73317
Iskhak <isakask12242001@gmail.com>
2022-11-22 01:23:32 +0300

    features: Ex2 well

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -1,5 +1,32 @@
-package ru.gb;public class Main {
+package ru.gb;
+
+import ru.gb.home5.ex2.EX2;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] strings = {
+                "Иван Иванов",
+                "Светлана Петрова",
+                "Кристина Белова",
+                "Анна Мусина",
+                "Анна Крутова",
+                "Иван Юрин",
+                "Петр Лыков",
+                "Павел Чернов",
+                "Петр Чернышов",
+                "Мария Федорова",
+                "Марина Светлова",
+                "Мария Савина",
+                "Мария Рыкова",
+                "Марина Лугова",
+                "Анна Владимирова",
+                "Иван Мечников",
+                "Петр Петин",
+                "Иван Ежов",
+        };
+        EX2 ex2 = new EX2();
+        ex2.createList(strings);
+        System.out.println(ex2.getNames());
+        ex2.getPopularName();
     }
 }
